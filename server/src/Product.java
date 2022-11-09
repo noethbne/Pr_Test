0339d8b273a80df5b5b359a563cb38ef7e420eea
Bene Noeth <noeth.benedikt@gmail.com>
2022-11-09 17:31:26 +0100

    Commit

--- server/src/Product.java
+++ server/src/Product.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public interface Product {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Product extends Remote{
+    public String getName() throws RemoteException;
+    public String getDescription() throws RemoteException;
+    public double getPrice() throws RemoteException;
 }
--- server/src/ProductImpl.java
+++ server/src/ProductImpl.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ProductImpl {
+import java.rmi.RemoteException;
+
+public class ProductImpl implements Product{
+    public String name;
+    public String description;
+    public double price;
+
+    public ProductImpl(String newName, String newDescription, double newPrice)throws RemoteException{
+        this.name = newName;
+        this.description = newDescription;
+        this.price = newPrice;
+    }
+
+    public String getName() throws RemoteException{
+        return this.name;
+    }
+    public String getDescription() throws RemoteException{
+        return this.description;
+    }
+    public double getPrice() throws  RemoteException{
+        return this.price;
+    }
 }
