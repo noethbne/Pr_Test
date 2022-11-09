a9eff64866d321195d72e9d9c0e1d76a92d5aa89
Bene Noeth <noeth.benedikt@gmail.com>
2022-11-09 17:59:58 +0100

    ~

--- server/src/Product.java
+++ server/src/Product.java
@@ -1,8 +1,2 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Product extends Remote{
-    public String getName() throws RemoteException;
-    public String getDescription() throws RemoteException;
-    public double getPrice() throws RemoteException;
+package PACKAGE_NAME;public interface Product {
 }
